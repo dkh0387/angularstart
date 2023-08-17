e0d83a7d0f93f0ceb8ef57e2ddfc944e2f26d8f0
dkh0387 <deniskh87@gmail.com>
2023-08-17 21:00:08 +0200

    friends proposals rearranged on hamster card height

--- my-first-project/src/app/profile-row/profile-row.component.ts
+++ my-first-project/src/app/profile-row/profile-row.component.ts
@@ -1,14 +1,15 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-profile-row',
-  templateUrl: './profile-row.component.html',
-  styleUrls: ['./profile-row.component.scss']
+    selector: 'app-profile-row',
+    templateUrl: './profile-row.component.html',
+    styleUrls: ['./profile-row.component.scss']
 })
 export class ProfileRowComponent {
-  @Input() imgPath: string = '';
-  @Input() imgAlt: string = '';
-  @Input() name: string = '';
-  @Input() description: string = '';
+    @Input() imgPath: string = '';
+    @Input() imgAlt: string = '';
+    @Input() name: string = '';
+    @Input() description: string = '';
+    @Input() followLink: string = '';
 
 }
