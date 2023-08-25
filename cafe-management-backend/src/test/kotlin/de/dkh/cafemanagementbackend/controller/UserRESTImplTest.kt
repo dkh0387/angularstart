7db0b5dcf142e85318e234ed4879bc7e609d7d1f
dkh0387 <deniskh87@gmail.com>
2023-08-25 17:28:34 +0200

    Generating jwt in tests in order to pass the filter: all tests are green

--- cafe-management-backend/src/test/kotlin/de/dkh/cafemanagementbackend/controller/UserRESTImplTest.kt
+++ cafe-management-backend/src/test/kotlin/de/dkh/cafemanagementbackend/controller/UserRESTImplTest.kt
@@ -17,9 +17,7 @@ import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
