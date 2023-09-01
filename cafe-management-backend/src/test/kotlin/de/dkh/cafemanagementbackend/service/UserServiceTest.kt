f4269663680a464fafc592774c52c02a6eda4e75
dkh0387 <deniskh87@gmail.com>
2023-09-01 13:32:22 +0200

    Adding a new category service testing

--- cafe-management-backend/src/test/kotlin/de/dkh/cafemanagementbackend/service/UserServiceTest.kt
+++ cafe-management-backend/src/test/kotlin/de/dkh/cafemanagementbackend/service/UserServiceTest.kt
@@ -4,7 +4,6 @@ import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
-import com.fasterxml.jackson.databind.ObjectMapper
 import de.dkh.cafemanagementbackend.constants.CafeConstants
 import de.dkh.cafemanagementbackend.entity.User
 import de.dkh.cafemanagementbackend.exception.*
