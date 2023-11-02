634fead4df76bc874be8826dcd6f733556f7ccae
dkh0387 <deniskh87@gmail.com>
2023-11-02 13:22:42 +0100

    category.component.spec.ts completed

--- cafe-management-frontend/src/app/material-component/dialog/category/category.component.spec.ts
+++ cafe-management-frontend/src/app/material-component/dialog/category/category.component.spec.ts
@@ -8,7 +8,6 @@ import {CategoryService} from "../../../services/category.service";
 import {GlobalConstants} from "../../../shared/global-constants";
 import {By} from "@angular/platform-browser";
 import {Observable} from "rxjs";
-import {UserService} from "../../../services/user.service";
 import {HttpClient} from "@angular/common/http";
 
 
@@ -72,7 +71,6 @@ describe('CategoryComponent', () => {
   });
 
   it('should have a dialog title according to the corresponding dialogData', () => {
-    const categoryForm = fixture.debugElement.nativeElement.querySelector('#categoryForm');
     const title = fixture.debugElement.query(By.css("#title")).nativeElement;
     expect(title.innerHTML).toBe(GlobalConstants.dialogActionEdit);
   });
@@ -92,7 +90,6 @@ describe('CategoryComponent', () => {
   });
 
   it('should throw an error if the category service does while update a category', () => {
-    const data = {"id": 1, "name": "Drinks"};
     component.categoryForm.setValue({
       "name": "Drinks",
     });
