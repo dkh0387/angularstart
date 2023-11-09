24664c577b446f9d3151f7a2d9151abc45f5fcf6
dkh0387 <deniskh87@gmail.com>
2023-11-09 09:56:48 +0100

    BREAKING CHANGE: angular 13 updated

--- cafe-management-frontend/src/app/services/bill.service.ts
+++ cafe-management-frontend/src/app/services/bill.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {RequestService} from "./extended/request.service";
 import {HttpClient} from "@angular/common/http";
 import {GlobalConstants} from "../shared/global-constants";
-import {Observable} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
