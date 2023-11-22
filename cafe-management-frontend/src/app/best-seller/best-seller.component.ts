b3a81fb504917bfa8f417488a9bdcbd355290517
dkh0387 <deniskh87@gmail.com>
2023-11-22 11:58:55 +0100

    html formatted best seller descriptions

--- cafe-management-frontend/src/app/best-seller/best-seller.component.ts
+++ cafe-management-frontend/src/app/best-seller/best-seller.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {GlobalConstants} from "../shared/global-constants";
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {BuyComponent} from "../material-component/dialog/buy/buy.component";
